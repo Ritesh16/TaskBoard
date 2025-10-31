e27590a1485b1a12c6d4bf92257f03575b18639e
Ritesh16 <ritesh.sharma.cse@gmail.com>
2025-10-30 22:12:17 -0400

    Fixing design of application

---
+++ TaskBoard.Web/src/app/layout/NavBar.tsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg" fixed="top" className="w-100">
+      <Container fluid>
+        <Navbar.Brand href="#home">Task Board</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#boards">Boards</Nav.Link>
+            <NavDropdown title="More" id="nav-dropdown">
+              <NavDropdown.Item href="#/action-1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#/action-2">Another action</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
