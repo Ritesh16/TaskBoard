3c6a4c6fad261595c50c67fd45763f33ea9581ba
Ritesh16 <ritesh.sharma.cse@gmail.com>
2026-01-19 22:14:12 -0500

    Add code to get scheduled Tasks

---
+++ TaskBoard.Dto/Constants/RepeatType.cs
@@ -0,0 +1,15 @@
+namespace TaskBoard.Dto.Constants
+{
+    /// <summary>
+    /// Represents task repeat types to eliminate magic strings.
+    /// </summary>
+    public static class RepeatType
+    {
+        public const string OneTime = "OneTime";
+        public const string Daily = "Daily";
+        public const string Weekly = "Weekly";
+        public const string Monthly = "Monthly";
+        public const string Yearly = "Yearly";
+        public const string Custom = "Custom";
+    }
+}
--- TaskBoard.Service/ScheduledTasksService.cs
+++ TaskBoard.Service/ScheduledTasksService.cs
@@ -1,9 +1,7 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using TaskBoard.Data.Interfaces;
 using TaskBoard.Dto;
+using TaskBoard.Dto.Constants;
 using TaskBoard.Service.Interfaces;
 
 namespace TaskBoard.Service
@@ -18,11 +16,153 @@ namespace TaskBoard.Service
             _scheduledTasksRepository = scheduledTasksRepository;
             _mapper = mapper;
         }
+
         public async Task<IEnumerable<UserTaskDto>> GetScheduledTasksForToday(int userId)
         {
             var scheduledTasks = await _scheduledTasksRepository.GetTasks(userId);
             var scheduledTasksDto = _mapper.Map<IEnumerable<UserTaskDto>>(scheduledTasks);
-            return scheduledTasksDto;
+
+            return scheduledTasksDto
+                .Where(task => task.Schedule != null)
+                .Where(task => !HasCompletedToday(task))
+                .Where(ShouldScheduleTaskToday)
+                .ToList();
         }
+
+        private bool ShouldScheduleTaskToday(UserTaskDto task)
+        {
+            return task.Schedule.Repeat switch
+            {
+                RepeatType.OneTime => IsOneTimeTaskDue(task),
+                RepeatType.Daily => IsRecurringTaskActive(task),
+                RepeatType.Weekly => IsWeeklyTaskDue(task),
+                RepeatType.Monthly => IsMonthlyTaskDue(task),
+                RepeatType.Yearly => IsYearlyTaskDue(task),
+                RepeatType.Custom => IsCustomTaskDue(task),
+                _ => false
+            };
+        }
+
+        private bool IsOneTimeTaskDue(UserTaskDto task)
+        {
+            return task.Schedule.StartDate.Date == DateTime.Now.Date;
+        }
+
+        private bool IsRecurringTaskActive(UserTaskDto task)
+        {
+            return IsTaskWithinDateRange(task) && IsWithinEndAfterLimit(task);
+        }
+
+        private bool IsWeeklyTaskDue(UserTaskDto task)
+        {
+            if (!IsRecurringTaskActive(task))
+                return false;
+
+            return task.Schedule.SelectedDays.Any(day => 
+                DateTime.Now.DayOfWeek == (DayOfWeek)day);
+        }
+
+        private bool IsMonthlyTaskDue(UserTaskDto task)
+        {
+            if (!IsRecurringTaskActive(task))
+                return false;
+
+            var nextExecutionDate = GetNextExecutionDateForMonthly(task);
+            return nextExecutionDate.Date == DateTime.Now.Date;
+        }
+
+        private bool IsYearlyTaskDue(UserTaskDto task)
+        {
+            if (!IsRecurringTaskActive(task))
+                return false;
+
+            var nextExecutionDate = GetNextExecutionDateForYearly(task);
+            return nextExecutionDate.Date == DateTime.Now.Date;
+        }
+
+        private bool IsCustomTaskDue(UserTaskDto task)
+        {
+            if (!IsRecurringTaskActive(task))
+                return false;
+
+            var nextExecutionDate = task.Schedule.StartDate;
+            var interval = Convert.ToInt32(task.Schedule.CustomUnit);
+          
+            var lastExecution = GetLastTaskInstance(task);
+
+            if (lastExecution != null)
+            {
+                nextExecutionDate = GetNextExecutionDate(task.Schedule.CustomRepeat, lastExecution.CompletedDate, interval);
+            }
+
+            return nextExecutionDate.Date == DateTime.Now.Date;
+        }
+
+        private bool IsTaskWithinDateRange(UserTaskDto task)
+        {
+            var today = DateTime.Now.Date;
+            return task.Schedule.StartDate.Date <= today &&
+                   (task.Schedule.EndDate == null || task.Schedule.EndDate.Value.Date >= today);
+        }
+
+        private bool IsWithinEndAfterLimit(UserTaskDto task)
+        {
+            if (task.Schedule.EndAfter == null)
+                return true;
+
+            var maxInstances = Convert.ToInt32(task.Schedule.EndAfter);
+            var currentInstanceCount = task.Instances.Count(x => x.TaskId == task.TaskId);
+            return currentInstanceCount < maxInstances;
+        }
+
+        private bool HasCompletedToday(UserTaskDto task)
+        {
+            return task.Instances.Any(x => 
+                x.TaskId == task.TaskId && 
+                x.CompletedDate.Date == DateTime.Now.Date);
+        }
+
+        private DateTime GetNextExecutionDateForMonthly(UserTaskDto task)
+        {
+            var lastInstance = GetLastTaskInstance(task);
+            if (lastInstance != null)
+            {
+                return GetNextExecutionDate("months", lastInstance.CompletedDate, 1);
+            }
+
+            return task.Schedule.StartDate.AddMonths(1);
+        }
+
+        private DateTime GetNextExecutionDateForYearly(UserTaskDto task)
+        {
+            var lastInstance = GetLastTaskInstance(task);
+            if (lastInstance != null)
+            {
+                return GetNextExecutionDate("years", lastInstance.CompletedDate, 1);
+            }
+
+            return task.Schedule.StartDate.AddYears(1);
+        }
+
+        private TaskInstanceDto GetLastTaskInstance(UserTaskDto task)
+        {
+            if(task.Instances == null || !task.Instances.Any())
+                return null;
+
+            return task.Instances
+                .Where(x => x.TaskId == task.TaskId)
+                .OrderByDescending(x => x.TaskInstanceId)
+                .FirstOrDefault();
+        }
+
+        private DateTime GetNextExecutionDate(string repeatOn, DateTime lastExecutionDate, int interval) => 
+            repeatOn.ToLower() switch
+            {
+                "weeks" => lastExecutionDate.AddDays(7 * interval),
+                "months" => lastExecutionDate.AddMonths(interval),
+                "years" => lastExecutionDate.AddYears(interval),
+                "days" => lastExecutionDate.AddDays(interval),
+                _ => lastExecutionDate
+            };
     }
 }
